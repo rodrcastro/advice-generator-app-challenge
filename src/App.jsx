93f61e66a5afe883c6b3e8843f8e313ebc64c3a4
Rodrigo <rodrcastro@gmail.com>
2022-03-13 21:13:15 +0000

    set components to main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import Card from './components/Card/card'
+import Footer from './components/Footer/footer'
 
 function App() {
   return (
-    <div className="text-darkgray-blue">
-        Advice #Advice ID goes here
-
-        "Advice text goes here"
-
-      <div class="attribution">
-      Challenge by 
-      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
-        >Frontend Mentor</a
-      >. Coded by <a href="#">Your Name Here</a>.
-      </div>
-    </div>
+    <>
+      <Card />
+      <Footer />
+    </>
   )
 }
 
